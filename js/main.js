75d1460850acf99c5cc642bea6a482fd84464cbc
Rajesh000730 <rajeshrachit09@gmail.com>
2021-09-03 22:38:24 +0530

    test6

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
 window.onload = ()=>{
-    document.addEventListener("DOMContentLoaded", function() {
-        var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
-      
-        if ("IntersectionObserver" in window) {
-          let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
-              if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                lazyBackgroundObserver.unobserve(entry.target);
-              }
-            });
-          });
-      
-          lazyBackgrounds.forEach(function(lazyBackground) {
-            lazyBackgroundObserver.observe(lazyBackground);
-          });
-        }
-      });
+    document.getElementById('container').fadeIn(1000)
 }
